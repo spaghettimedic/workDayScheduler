8e40ba2b7d2f33f014ccdcb1ac5dadffa59ffa14
spaghettimedic <dsteiner247@gmail.com>
2021-11-06 20:00:38 -0400

    started building page

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var bodyEl = document.querySelector("body")
+var containerEl = document.querySelector(".container");
+
+var taskAudit = function(taskEl) { // review module 5.4.6 for this part
+    // get time from .time div
+
+
+    // remove old classes from element (for background color)
+
+
+    // add new classes to element (for bacckground color) if task is at or after hour accordingly
+}

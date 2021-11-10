5409ceaa959c1e48e53111c7d28521cfa01ff4fa
spaghettimedic <dsteiner247@gmail.com>
2021-11-10 09:42:23 -0500

    fixed bg-color persistence bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ var createTimeBlocks = function() {
         };
 
         var taskTimeEl = $("<div>").addClass("taskTime col-1 py-4 border border-secondary border-left-0 height-66").attr("id", "time" + [i]).html(taskTimes[i]);
-        var taskEl = $("<div>").addClass("task col-10 py-4 border-top border-bottom border-white height-66").attr("id", "task" + [i]).text(savedTasks[i]);
+        var taskEl = $("<div>").addClass("task text-white col-10 py-4 border-top border-bottom border-white height-66").attr("id", "task" + [i]).text(savedTasks[i]);
         var saveButtonEl = $("<button>").addClass("col-1 btn btn-info border-top border-bottom border-white rounded-lg rounded-right height-66").html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16"><path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/></svg>');
         // all new elements are appended to the .container div in the <body>
         $(containerEl).append(taskTimeEl, taskEl, saveButtonEl);
@@ -76,17 +76,19 @@ var auditTask = function() {
 // .task <div> was clicked to make it editable
 $(".container").on("click", ".task", function() {
     var text = $(this).text().trim();
-    var textInput = $("<textarea>").addClass("form-control col-10").val(text);
+    var id = $(this)[0].id
+    var textInput = $("<textarea>").addClass("form-control col-10").attr("id", id).val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
 
 // save button was clicked to save edited field and change it back into a <div> from a <textarea> element
 $(".container").on("click", ".btn", function() {
-    var text = $(".form-control").val().trim();
-    var editedTask = $("<div>").addClass("task col-10 py-4 border-top border-bottom border-white height-66").text(text);
+    var id = $(this).siblings("textarea")[0].id
+    var text = $("#" + id).val();
+    var editedTask = $("<div>").addClass("task text-white col-10 py-4 border-top border-bottom border-white height-66").attr("id", id).text(text);
 
-    $(".form-control").replaceWith(editedTask);
+    $(this).siblings("textarea").replaceWith(editedTask);
     // savedTasks = ($(".task").text());
     // console.log(savedTasks);
     // localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
